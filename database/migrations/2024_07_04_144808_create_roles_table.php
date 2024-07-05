1a190064da894893f69f21827cc92145dae84b11
pinof <fattoruso.g@gmail.com>
2024-07-05 11:54:15 +0200

    migrazione di roles + dati

--- database/migrations/2024_07_04_144808_create_roles_table.php
+++ database/migrations/2024_07_04_144808_create_roles_table.php
@@ -3,18 +3,29 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Role;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
+
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('role');
             $table->timestamps();
+        });
+
+
+        $roles = collect([
+            ["role" => "amministratore"],
+            ["role" => "data-entry"],
+            ["role" => "user"]
+        ]);
+        $roles->each(function ($role) {
+            Role::create($role);
         });
     }
 

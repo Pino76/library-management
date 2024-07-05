b6c9646bf1d0c757b96fef25cc97253fa18e77b5
pinof <fattoruso.g@gmail.com>
2024-07-05 11:53:41 +0200

    registazione di interfacce e classi di book e genre

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use app\Interfaces\Repository\IBookRepository;
+use app\Interfaces\Repository\IGenreRepository;
+use app\Interfaces\Service\IBookService;
+use app\Interfaces\Service\IGenreService;
+use app\Repository\BookRepository;
+use app\Repository\GenreRepository;
+use app\Services\BookService;
+use app\Services\GenreService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(IBookService::class, BookService::class);
+        $this->app->singleton(IBookRepository::class, BookRepository::class);
+        $this->app->singleton(IGenreService::class, GenreService::class);
+        $this->app->singleton(IGenreRepository::class, GenreRepository::class);
     }
 
     /**
